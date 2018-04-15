6f24ff6678982eb4e1a4e341e21b0eea9963b602
Josh <switch620@gmail.com>
2018-04-14 17:41:35 -0700

    can write length (bits) to end of image

--- main.go
+++ main.go
@@ -7,92 +7,161 @@ import (
 	_ "image/png"
 	"io"
 	"log"
+	"math"
 	"os"
 	"strings"
-	"math"
 )
 
-// var file_name = "test.png"
-var file_name = "testImage.png"
-
-func b2d(input []byte) float64 {
-	result := 0.0
-	for i, val := range(input) {
-		power := len(input) - 1 - i
-		if val == 1 {
-			result += math.Pow(2, float64(power))
-		}
-		fmt.Println(power, result)
-		}
-	return result
-}
-
-func main() {
-	pixel_array := get_image()
-	fmt.Println(len(pixel_array))
-	// for _, val := range pixel_array[len(pixel_array)-1][len(pixel_array[0])-11:] {
-	fmt.Println("Last values")
-	var leng []byte
-	for i := range pixel_array[len(pixel_array)-11:] {
-		fmt.Println(i)
-		leng = append(leng, byte(pixel_array[len(pixel_array)-i-1].R&1))
-		leng = append(leng, byte(pixel_array[len(pixel_array)-i-1].G&1))
-		leng = append(leng, byte(pixel_array[len(pixel_array)-i-1].B&1))
-	}
-	// output := binary.BigEndian.Uint32(leng[:len(leng)-1])
-	// output := uint32(leng[:len(leng)-1])
-	fmt.Println(leng[:len(leng)-1])
-	fmt.Println(b2d(leng[:len(leng)-1]))
-	new_string := fmt.Sprintf("%08b", []byte("ab ab"))         // Pad with leading 0s
-	replacer := strings.NewReplacer("[", "", "]", "", " ", "") // Stripping [, ], and whitespace
-	new_string = replacer.Replace(new_string)
-	fmt.Println("\noutput: ", new_string)
-
-}
-
-func get_image() []Pixel {
-	file, err := os.Open(file_name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	pixel_array, err := get_pixel_array(file)
-	return pixel_array
-
-}
+// var fileName = "test.png"
+var fileName = "testImage.png"
 
 type Pixel struct {
 	R, G, B, A int
 }
 
-func convert_to_pixel(r uint32, g uint32, b uint32, a uint32) Pixel {
-	// Takes in a golang pixel in rgba format
-	// and converts all the values from uint32s to normal integers
-	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+func main() {
+	// TESTING STRING TO BINARY
+	newString := fmt.Sprintf("%08b", []byte("ab ab"))          // Pad with leading 0s
+	replacer := strings.NewReplacer("[", "", "]", "", " ", "") // Stripping [, ], and whitespace
+	newString = replacer.Replace(newString)
+	messageLength := len(newString)
+	temp := fmt.Sprintf("%b", messageLength)
+	fmt.Println([]byte(temp))
+	fmt.Println("\noutput: ", newString, len(newString))
+
+	fmt.Println(i2b(len(newString)))
+	bitLength := i2b(len(newString))
+	pixelArray := getImage()
+	fmt.Println("Testing...")
+	// Have to add cases to make sure it is doing the right boolean logic for each portion... atm it's only ORing 1.
+	//		This is only helpful when you are writing a 1, not when you need to write a 0 (& 0)
+	for i := 0; i < 11; i++ {
+		if int(bitLength[i]) == 1 {
+			pixelArray[len(pixelArray)-1-i].R = pixelArray[len(pixelArray)-1-i].R | int(bitLength[i*3])
+		} else {
+			pixelArray[len(pixelArray)-1-i].R = pixelArray[len(pixelArray)-1-i].R & 254
+		}
+		if int(bitLength[(i*3)+1]) == 1 {
+			pixelArray[len(pixelArray)-1-i].G = pixelArray[len(pixelArray)-1-i].G | int(bitLength[(i*3)+1])
+		} else {
+			pixelArray[len(pixelArray)-1-i].G = pixelArray[len(pixelArray)-1-i].G & 254
+		}
+		if i == 10 {
+			continue
+		}
+
+		if int(bitLength[(i*3)+2]) == 1 {
+			pixelArray[len(pixelArray)-1-i].B = pixelArray[len(pixelArray)-1-i].B | int(bitLength[(i*3)+2])
+		} else {
+			pixelArray[len(pixelArray)-1-i].B = pixelArray[len(pixelArray)-1-i].B & 254
+		}
+		// fmt.Print(" ", pixelArray[len(pixelArray)-1-i].B&1, ":", pixelArray[len(pixelArray)-1-i].B&1, ",")
+	}
+	fmt.Print("\n", pixelArray[len(pixelArray)-11:])
+
 }
 
-func get_pixel_array(file io.Reader) ([]Pixel, error) {
-	loaded_image, _, err := image.Decode(file)
+func extract() {
+	// Open the file and get the pixel array
+	pixelArray := getImage()
+
+	// Getting stored message length
+	var leng []byte
+	for i := range pixelArray[len(pixelArray)-11:] {
+		fmt.Println(i)
+		leng = append(leng, byte(pixelArray[len(pixelArray)-i-1].R&1))
+		leng = append(leng, byte(pixelArray[len(pixelArray)-i-1].G&1))
+		leng = append(leng, byte(pixelArray[len(pixelArray)-i-1].B&1))
+	}
+
+	messageLength := b2d(leng[:len(leng)-1])
+
+	// Debugging-- Getting len
+	fmt.Println("Message length: ", messageLength/8, "Characters.")
+
+	// Retreive the message pixels
+	var tmpMessage []byte
+	for i := range pixelArray[(len(pixelArray) - (11 + int(math.Ceil(messageLength/3)))) : len(pixelArray)-11] {
+		tmpMessage = append(tmpMessage, byte(pixelArray[len(pixelArray)-i-12].R&1))
+		tmpMessage = append(tmpMessage, byte(pixelArray[len(pixelArray)-i-12].G&1))
+		tmpMessage = append(tmpMessage, byte(pixelArray[len(pixelArray)-i-12].B&1))
+	}
+
+	// Retreive their bits
+	var message []byte
+	for i := 0; i < int(messageLength/8); i++ {
+		var char byte = 0
+		for j := 0; j < 8; j++ {
+			// char = append(char, byte(tmpMessage[(i*8)+j]))
+			char = (char << 1) + tmpMessage[(i*8)+j]
+		}
+		message = append(message, char)
+	}
+	// Converting
+	fmt.Print(string(message))
+
+}
+
+func getImage() []Pixel {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	pixelArray, err := getPixelArray(file)
+	return pixelArray
+
+}
+
+func getPixelArray(file io.Reader) ([]Pixel, error) {
+
+	loadedImage, _, err := image.Decode(file)
 	if err != nil {
 		fmt.Println("woops")
 		log.Fatal(err)
 	}
 
-	height := loaded_image.Bounds().Dy()
-	width := loaded_image.Bounds().Dx()
-	fmt.Println(loaded_image.At(width-1, height-1).RGBA())
-	fmt.Println(width, height)
+	height := loadedImage.Bounds().Dy()
+	width := loadedImage.Bounds().Dx()
 	var pixels []Pixel
+	// fmt.Println(loadedImage.At(width-1, height-1).RGBA())
+	// fmt.Println(width, height)
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			pixels = append(pixels, convert_to_pixel(loaded_image.At(x, y).RGBA()))
-			// if x == width-1 && y == height-1 {
-			// 	fmt.Println(row[width-1])
-			// }
+			pixels = append(pixels, toPixel(loadedImage.At(x, y).RGBA()))
 		}
 	}
 
 	return pixels, nil
+}
+
+func toPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	// Takes in a golang pixel in rgba format
+	// and converts all the values from uint32s to normal integers
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+func b2d(input []byte) float64 {
+	result := 0.0
+	for i, val := range input {
+		power := len(input) - 1 - i
+		if val == 1 {
+			result += math.Pow(2, float64(power))
+		}
+		fmt.Println(power, result)
+	}
+	return result
+}
+
+func i2b(input int) [32]byte {
+	var result [32]byte
+	for i := 0; i < 32; i++ {
+		if (input & 1) == 1 {
+			result[31-i] = 1
+		}
+		input = input >> 1
+	}
+	return result
 }
