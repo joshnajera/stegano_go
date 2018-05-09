9164f6a880994130186ad924a02ce372fe42ecba
Josh <switch620@gmail.com>
2018-05-09 11:13:34 -0700

    Text writing functionality added

--- main.go
+++ main.go
@@ -10,12 +10,23 @@ import (
 	"log"
 	"math"
 	"os"
-	"strings"
 )
 
 // var fileName = "test.png"
 
 var fileName = "image.png"
+
+func convertString(input string) []byte {
+	var temp []byte
+	for _, char := range input {
+		for _, bytes := range i2b(int(char), 8) {
+			// Convert each character's ord value to bits
+			temp = append(temp, bytes)
+		}
+	}
+	// fmt.Println(temp)
+	return temp
+}
 
 type Pixel struct {
 	R, G, B, A int
@@ -24,61 +35,30 @@ type Pixel struct {
 func main() {
 
 	// TESTING STRING TO BINARY
-	newString := fmt.Sprintf("%08b", []byte("ssss"))           // Pad with leading 0s
-	replacer := strings.NewReplacer("[", "", "]", "", " ", "") // Stripping [, ], and whitespace that are added while Sprintf'ing
-	newString = replacer.Replace(newString)
-	binaryString := s2b(newString)
-	fmt.Println("\nMessage as bytes and its len: ", binaryString, len(binaryString))
-	bitLength := i2b(len(binaryString))
-	fmt.Println("Message len in bits: ", (bitLength))
-
-	fmt.Println("Quick test")
-	fmt.Println("\nBinary repr of string len", bitLength)
+	testString := "and then there was one"
+	newString := convertString(testString)
+	// newString := fmt.Sprintf("%08b", []byte("ssss")) // Pad with leading 0s
+	// fmt.Println(newString)
+	// replacer := strings.NewReplacer("[", "", "]", "", " ", "") // Stripping [, ], and whitespace that are added while Sprintf'ing
+	// newString = replacer.Replace(newString)
+	// binaryString := s2b(newString)
+	fmt.Println("\nMessage as bytes and its len:\n", newString, len(newString))
+	bitLength := i2b(len(newString), 32)
+	fmt.Println("Message len in bits:\n", bitLength)
 
 	// Load fileName and create a placeholder with same dimensions
 	pixelArray, width, height := getImage()
 	img := image.NewNRGBA(image.Rect(0, 0, width, height))
 
-	fmt.Println("Testing...")
 	// Have to add cases to make sure it is doing the right boolean logic for each portion... atm it's only ORing 1.
 	//		This is only helpful when you are writing a 1, not when you need to write a 0 (& 0)
-	fmt.Println("\nPrinting the last 11 pixels", pixelArray[len(pixelArray)-11:])
+	fmt.Println("Printing the last 11 pixels\n", pixelArray[len(pixelArray)-11:])
 
+	// Write the length of the message
 	writeBits(0, pixelArray, bitLength)
+	writeBits(11, pixelArray, newString)
 
-	fmt.Println("\nPrinting the last 11 pixels", pixelArray[len(pixelArray)-11:])
-	// for i := 0; i < 11; i++ {
-	// 	fmt.Print(" r", i, ":")
-	// 	if int(bitLength[i*3]) == 1 {
-	// 		pixelArray[len(pixelArray)-1-i].R = pixelArray[len(pixelArray)-1-i].R | int(bitLength[i*3])
-	// 	} else {
-	// 		pixelArray[len(pixelArray)-1-i].R = pixelArray[len(pixelArray)-1-i].R & 254
-	// 	}
-	// 	fmt.Print(pixelArray[len(pixelArray)-1-i].R)
-
-	// 	fmt.Print(" g", i, ":")
-	// 	if int(bitLength[(i*3)+1]) == 1 {
-	// 		pixelArray[len(pixelArray)-1-i].G = pixelArray[len(pixelArray)-1-i].G | int(bitLength[(i*3)+1])
-	// 	} else {
-	// 		pixelArray[len(pixelArray)-1-i].G = pixelArray[len(pixelArray)-1-i].G & 254
-	// 	}
-	// 	fmt.Print(pixelArray[len(pixelArray)-1-i].G)
-
-	// 	if i == 10 {
-	// 		continue
-	// 	}
-
-	// 	fmt.Print(" b", i, ":")
-	// 	if int(bitLength[(i*3)+2]) == 1 {
-	// 		pixelArray[len(pixelArray)-1-i].B = pixelArray[len(pixelArray)-1-i].B | int(bitLength[(i*3)+2])
-	// 	} else {
-	// 		pixelArray[len(pixelArray)-1-i].B = pixelArray[len(pixelArray)-1-i].B & 254
-	// 	}
-	// 	fmt.Print(pixelArray[len(pixelArray)-1-i].B)
-	// 	// fmt.Print(" ", pixelArray[len(pixelArray)-1-i].B&1, ":", pixelArray[len(pixelArray)-1-i].B&1, ",")
-	// }
-	// fmt.Println("\nPrinting the last 11 pixels", pixelArray[len(pixelArray)-11:])
-
+	fmt.Println("\nPrinting the last 11 pixels\n", pixelArray[len(pixelArray)-11:])
 	fmt.Println("Number of pixels being used to write message: ", int(math.Ceil((float64(len(newString)) / 3))))
 
 	// Copying pixelArray to placeholder
@@ -110,36 +90,44 @@ func main() {
 func writeBits(offset int, pixelArray []Pixel, source []byte) []Pixel {
 	numPix := int(math.Ceil(float64(len(source)) / 3))
 	arrayLen := len(pixelArray)
+	sourceLen := len(source)
 
+	fmt.Println("")
 	for i := 0; i < numPix; i++ {
+		position := arrayLen - 1 - offset - i
 		fmt.Print(" r", i, ":")
 		if int(source[i*3]) == 1 {
-			pixelArray[arrayLen-(offset+1)-i].R = pixelArray[arrayLen-(offset+1)-i].R | int(source[i*3])
+			pixelArray[position].R = pixelArray[position].R | int(source[i*3])
 		} else {
-			pixelArray[arrayLen-(offset+1)-i].R = pixelArray[arrayLen-(offset+1)-i].R & 254
+			pixelArray[position].R = pixelArray[position].R & 254
 		}
-		fmt.Print(pixelArray[arrayLen-(offset+1)-i].R)
+		fmt.Print(pixelArray[position].R)
+
+		if (i*3)+1 >= sourceLen {
+			break
+		}
 
 		fmt.Print(" g", i, ":")
-		if int(source[(i*3)+(offset+1)]) == 1 {
-			pixelArray[arrayLen-(offset+1)-i].G = pixelArray[arrayLen-(offset+1)-i].G | int(source[(i*3)+1])
+		if int(source[(i*3)+1]) == 1 {
+			pixelArray[position].G = pixelArray[position].G | int(source[(i*3)+1])
 		} else {
-			pixelArray[arrayLen-(offset+1)-i].G = pixelArray[arrayLen-(offset+1)-i].G & 254
+			pixelArray[position].G = pixelArray[position].G & 254
 		}
-		fmt.Print(pixelArray[arrayLen-(offset+1)-i].G)
+		fmt.Print(pixelArray[position].G)
 
-		if i == 10 {
-			continue
+		if (i*3)+2 >= sourceLen {
+			break
 		}
 
 		fmt.Print(" b", i, ":")
 		if int(source[(i*3)+2]) == 1 {
-			pixelArray[arrayLen-(offset+1)-i].B = pixelArray[arrayLen-(offset+1)-i].B | int(source[(i*3)+2])
+			pixelArray[position].B = pixelArray[position].B | int(source[(i*3)+2])
 		} else {
-			pixelArray[arrayLen-(offset+1)-i].B = pixelArray[arrayLen-(offset+1)-i].B & 254
+			pixelArray[position].B = pixelArray[position].B & 254
 		}
-		fmt.Print(pixelArray[arrayLen-(offset+1)-i].B)
+		fmt.Print(pixelArray[position].B)
 	}
+	fmt.Println("")
 	return pixelArray
 }
 
@@ -231,20 +219,19 @@ func b2d(input []byte) float64 {
 		if val == 1 {
 			result += math.Pow(2, float64(power))
 		}
-		fmt.Println(power, result)
 	}
 	return result
 }
 
-func i2b(input int) []byte {
+func i2b(input int, numBytes int) []byte {
 	// Converts an integer to binary
 	var result []byte
-	for i := 0; i < 32; i++ {
+	for i := 0; i < numBytes; i++ {
 		result = append(result, 0)
 	}
-	for i := 0; i < 32; i++ {
+	for i := 0; i < numBytes; i++ {
 		if (input & 1) == 1 {
-			result[31-i] = 1
+			result[numBytes-1-i] = 1
 		}
 		input = input >> 1
 	}
